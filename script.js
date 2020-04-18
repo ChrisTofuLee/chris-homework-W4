6c3ff94a03def793e1a0c3a82e9512a1bc5b48c6
Selfay <chris.lee90@hotmail.co.uk>
2020-04-19 00:12:48 +0100

    adding in timer placeholder and layout of homescreen

---
+++ script.js
@@ -0,0 +1,54 @@
+
+
+const Questions = [
+  {
+    questionId: 1,
+    qOne: "",
+    qTwo: "",
+    qThree: "",
+    qFour: "",
+    answer: "",
+  },
+  {
+    questionId: 2,
+    qOne: "",
+    qTwo: "",
+    qThree: "",
+    qFour: "",
+    answer: "",
+  },
+  {
+    questionId: 3,
+    qOne: "",
+    qTwo: "",
+    qThree: "",
+    qFour: "",
+    answer: "",
+  },
+  {
+    questionId: 4,
+    qOne: "",
+    qTwo: "",
+    qThree: "",
+    qFour: "",
+    answer: "",
+  },
+  {
+    questionId: 5,
+    qOne: "",
+    qTwo: "",
+    qThree: "",
+    qFour: "",
+    answer: "",
+  },
+];
+
+const titleInfo = [
+    {
+        title: "The Coding Quiz"
+        theBody: "Test your coding knowledge with the following questions within the time limit. Think twice before clicking though as incorrect answers will penalize your score as well as the time you have left by 10 seconds!"
+        toStart: "Start Quiz"  
+    },
+]
+
+let questionIndex = 0;
